08dd2bf55297e57e79d0aed1084b7e9fba397cee
itzmarcos <marcozfuwa@gmail.com>
2026-02-27 10:10:57 -0300

    corrigindo o d19

--- des/d19/d19.py
+++ des/d19/d19.py
@@ -3,14 +3,18 @@ class Livro:
     def __init__(self, titulo, paginas):
         self.titulo = titulo
         self.paginas = paginas
-        print(f'Você acabou de abrir o livro: {self.titulo} que contem {self.paginas} paginas no total.\nVocê agora esta na pagina 1')
-    def avancar(self, valor=0):
-        for c in range(1, valor+1):
+        self.valor = 0
+        # print(f'Você acabou de abrir o livro: {self.titulo} que contem {self.paginas} paginas no total.\nVocê agora esta na pagina 1')
+    def avancar(self, quant):       
+        for _ in range(quant):
             self.sleep(0.5)
-            print(f'Pág{c} ->', end=' ', flush=True)
-        print(f'Voce avançou {valor} paginas e agora esta na pagina {c}')
-            
+            print(f'Pág{self.valor+1} ->', end=' ', flush=True)
+            self.valor += 1
+        print(f'Voce avançou {self.valor} paginas e agora esta na pagina {self.valor}')
+        if self.valor > quant:
+            print('Voce atigiu o limite de pagina do livro')
+
 
 l1 = Livro('Batalha do Apocalipse', 10)
-l1.avancar(2)
-l1.avancar(5)
+l1.avancar(3)
+l1.avancar(15)
