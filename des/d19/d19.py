9fdbaad3210d3a33a430fb09ffb7666ca8ea8371
itzmarcos <marcozfuwa@gmail.com>
2026-03-03 15:53:02 -0300

    d19 finalizado

--- des/d19/d19.py
+++ des/d19/d19.py
@@ -4,16 +4,22 @@ class Livro:
         self.titulo = titulo
         self.paginas = paginas
         self.valor = 0
-        # print(f'Você acabou de abrir o livro: {self.titulo} que contem {self.paginas} paginas no total.\nVocê agora esta na pagina 1')
-    def avancar(self, quant):                      
+        print(f'\033[30mVocê acabou de abrir o livro: {self.titulo} que contem {self.paginas} paginas no total.\nVocê agora esta na\033[m\033[34m pagina 1\033[m')
+
+    def avancar(self, quant):                   
         for _ in range(quant):
+            if self.valor + 1 >= self.paginas:
+                print('\n\033[31mVocê chegou ao final do livro!\033[m')
+                break
+            
             self.sleep(0.5)
             print(f'Pág{self.valor+2} ->', end=' ', flush=True)
             self.valor += 1
-        print(f'\033[32mVoce avançou {quant} paginas e agora esta na\033[m \033[34mpagina {self.valor+1}\033[m')  
+        
+        print(f'\n\033[32mAgora você está na\033[m \033[34mpagina {self.valor+1}\033[m')
             
         
 l1 = Livro('Batalha do Apocalipse', 20)
 l1.avancar(5)
-l1.avancar(10)
-l1.avancar(6)
+l1.avancar(4)
+l1.avancar(100)
