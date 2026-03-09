0f20e041218ce1a720f1e04d3acf828535857223
itzmarcos <marcozfuwa@gmail.com>
2026-03-09 15:26:04 -0300

    d21, finalizado

--- des/d21/d21.py
+++ des/d21/d21.py
@@ -1,27 +1,26 @@
 #
 class Caneta:
+    cores = {
+        'Azul': '\033[34m',
+        'Vermelho': '\033[31m',
+        'Verde': '\033[32m'
+    }
+
     def __init__(self, cor):
         self.cor = cor
 
     def destampar(self):
         pass
 
-    def escrever(self, cor):
-        if self.cor == 'Azul' or 'Vermelho' or 'Verde':
-            print(f'\033[36m{cor}\033[m \033[31m{cor}\033[m {cor}')
+    def escrever(self, frase):
+        palavras = frase.split()
+        for palavra in palavras:
+            print(f"{Caneta.cores[self.cor]}{palavra}\033[m", end=' ')
 
 c1 = Caneta('Azul')
 c2 = Caneta('Vermelho')
 c3 = Caneta('Verde')
 
-c1.escrever('Olá, tudo bem?')
-c2.escrever('Olá, Gafanhoto!')
-c3.escrever('Vamos exercitar!')
-
-
-# if self.cor == 'Azul':
-#         print(f'\033[36m{cor}\033[m')
-# if self.cor == 'Vermelho':
-#         print(f'\033[31m{cor}\033[m')
-# if self.cor == 'Verde':
-#         print(f'\033[32m{cor}\033[m')
+c1.escrever('Olá')
+c2.escrever('Mundo')
+c3.escrever('Tudo bem?')
