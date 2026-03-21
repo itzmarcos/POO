097ef2329b79ac19f0659d55ec55de9f7a3d33ee
itzmarcos <marcozfuwa@gmail.com>
2026-03-21 07:17:20 -0300

    01

--- des/d21/d21.py
+++ des/d21/d21.py
@@ -9,22 +9,31 @@ class Caneta:
             'Azul': '\033[36m',
             'Vermelho': '\033[31m',
             'Verde': '\033[32m'
-        }  
-
+        }
+        try:
+            if self.cor == 'Azul':
+              return '\033[36m'
+            if self.cor == 'Vermelho':
+              return '\033[31m'
+            if self.cor == 'Verde':
+              return '\033[32m'
+        except:
+           print('Caneta azul tampanda')
+        
     def escrever(self, frase):
         palavras = frase.split()
         for palavra in palavras:
-            print(f"{self.destampar()}{palavra}\033[m", end=' ')
+            print(f"{palavra}\033[m", end=' ')
 
 c1 = Caneta('Azul')
 c2 = Caneta('Vermelho')
 c3 = Caneta('Verde')
 
+c1.destampar()
 
-
-c1.escrever('Olá')
-c2.escrever('Mundo')
-c3.escrever('Tudo bem?')
+c1.escrever('A')
+c2.escrever('B')
+c3.escrever('C')
 
 
 
